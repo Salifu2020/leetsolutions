05b12ef48a4690fdfb09b37c4c48eb244748a961
Fuseini Salifu <sxf881@miami.edu>
2025-10-09 20:03:54 -0400

    Time: 0 ms (100%), Space: 17.7 MB (87.72%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+
+        nono = ''
+        for i in itertools.count(0):
+            if 4**i == n:
+                return True
+
+            if 2**i > n:
+                return False
+
+        return bool(nono)
+        
